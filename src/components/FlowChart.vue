fa4d4ba04620d4aac42c0e8a02881ae54c8ce88c
runtu <huorunquan@126.com>
2019-07-16 14:48:33 +0800

    IMP: 初始化组件

---
+++ src/components/FlowChart.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div ref="canvas"></div>
+  </div>
+</template>
+
+<script>
+let idx = 0
+
+export default {
+  data () {
+    return {}
+  },
+  mounted () {
+    this.initId()
+  },
+  methods: {
+    initId () {
+      this.$refs['canvas'].setAttribute('id', `cm_flowchart_${idx}`)
+    }
+  }
+}
+</script>
